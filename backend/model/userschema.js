88dfcb0bf1720c65d2efe5370dc9c1cc6609cddb
Kunjan Goyal <125953017+goyalkunjann@users.noreply.github.com>
2024-02-08 18:23:57 +0530

    Added login & signup

--- backend/model/userschema.js
+++ backend/model/userschema.js
@@ -14,10 +14,15 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true
     },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
     password: {
         type: String,
         required: true
     }
 });
 
-mongoose.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model("User", userSchema);
